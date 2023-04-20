55046c82eb9a057f64ef3b524d10a471fa4e4145
EC2 Default User <ec2-user@ip-172-31-95-0.ec2.internal>
2023-04-20 20:13:51 +0000

    update on new SSID

--- SignalProcess.py
+++ SignalProcess.py
@@ -6,6 +6,57 @@ locw = 0
 loch = 0
 cart1_loc = "Not sure where your cart is"
 
+def find_cart(mac_address):
+    w = 0
+    h = 0
+    loc = "Not sure where your cart is"
+    if("94:B4:0F:E6:34" in mac_address):
+        w = 10
+        h = 500
+        loc = "Phillips 238"
+
+    elif("F0:5C:19:6E:74" in mac_address):
+        w = 200
+        h = 520
+        loc = "Phillips 239"
+
+    elif("F0:5C:19:6E:A0" in mac_address):
+        w = 200
+        h = 420
+        loc = "Phillips 237"
+
+    elif("94:B4:0F:E6:35" in mac_address):
+        w = 30
+        h = 100
+        loc = "Phillips 200-1"
+
+    elif("94:B4:0F:E6:45:C" in mac_address):
+        w = 30
+        h = 180
+        loc = "Phillips 200-2"
+
+    elif("94:B4:0F:E6:44:E" in mac_address):
+        w = 300
+        h = 230
+        loc = "Phillips 200-3"
+
+    elif("E8:26:89:37:9B" in mac_address):
+        w = 5
+        h = 100
+        loc = "Phillips 203"
+
+    elif("E8:26:89:37:DB" in mac_address):
+        w = 5
+        h = 250
+        loc = "Phillips 219"
+
+    elif("94:B4:0F:E6:45:E" in mac_address):
+        w = 500
+        h = 230
+        loc = "Phillips 232"
+    
+    return w, h, loc
+
 if (os.path.getsize('wifi_signals.txt') == 0):
         locw = 300
         loch = 20
@@ -15,8 +66,6 @@ else:
         mac_list = []
         dB_list = []
         for line in f:
-            # average_strength = 0
-            # idx = 0
 
             # find mac address
             index = line.find(" ")
@@ -32,62 +81,22 @@ else:
                     # each number is seperated by a space
                     dB_list.append(int(num))
                     num = ""
-        max_num = max(dB_list)
-        max_index = dB_list.index(max_num)
-        mac_address = mac_list[max_index]
-        print(mac_address)
+        # max_num = max(dB_list)
+        # max_index = dB_list.index(max_num)
+        # mac_address = mac_list[max_index]
+        # print(mac_address)
 
-        if("94:B4:0F:E6:5F" in mac_address):
-            locw = 10
-            loch = 500
-            cart1_loc = "Phillips 238"
+        # record the current location
+        with open("path_history.txt", "w") as f:
+            for i in mac_list:
+                curr_locw, curr_loch, curr_cart1_loc = find_cart(i)
+                f.write("CART1 location: " + curr_cart1_loc + ", " + time.strftime('%l:%M%p %Z on %b %d, %Y'))
+                f.write('\n')
+        f.close()
 
-        elif("F0:5C:19:6E:74" in mac_address):
-            locw = 200
-            loch = 520
-            cart1_loc = "Phillips 239"
+        mac_address = mac_list[len(mac_list)-1]
+        locw, loch, cart1_loc = find_cart(mac_address)
 
-        elif("F0:5C:19:6E:A0" in mac_address):
-            locw = 200
-            loch = 420
-            cart1_loc = "Phillips 237"
-
-        elif("94:B4:0F:E6:35" in mac_address):
-            locw = 30
-            loch = 100
-            cart1_loc = "Phillips 200-1"
-
-        elif("94:B4:0F:E6:45:C" in mac_address):
-            locw = 30
-            loch = 180
-            cart1_loc = "Phillips 200-2"
-
-        elif("94:B4:0F:E6:44:E" in mac_address):
-            locw = 300
-            loch = 230
-            cart1_loc = "Phillips 200-3"
-
-        elif("E8:26:89:37:9B" in mac_address):
-            locw = 5
-            loch = 100
-            cart1_loc = "Phillips 203"
-
-        elif("E8:26:89:37:DB" in mac_address):
-            locw = 5
-            loch = 250
-            cart1_loc = "Phillips 219"
-
-        elif("94:B4:0F:E6:45:E" in mac_address):
-            locw = 500
-            loch = 230
-            cart1_loc = "Phillips 232"
-
-# record the current location
-with open("path_history.txt", "a") as f:
-    f.write("CART1 location: " + cart1_loc + ", " + time.strftime('%l:%M%p %Z on %b %d, %Y'))
-    f.write('\n')
-f.close()
-    
 # overwrite html file location.html
 with open("./static/location_template.html", "r") as template_f:
     with open ("./static/location.html", "w") as out_f:
@@ -99,7 +108,7 @@ template_f.close()
 
 # overwrite html file map.html
 with open("./static/map_template.html", "r") as template_f:
-    with open ("./static/map.html", "w") as out_f:
+    with open ("./static/p2_map.html", "w") as out_f:
         tmp = template_f.read()
         tmp = tmp.replace("LOCW", str(locw))
         tmp = tmp.replace("LOCH", str(loch))
--- app.py
+++ app.py
@@ -1,15 +1,27 @@
 from flask import Flask, request
 import os
+from collections import deque
 
 # app = Flask(__name__)
 app = Flask(__name__, static_folder='static', static_url_path='')
-
+history = 0
+mac = deque()
+dB = deque()
 # @app.route("/")
 # def checklive():
 #     return "owendpersonal.com is live"
 
 @app.route("/", methods=['GET', 'POST'])
 def hello_world():
+    if(request.method == 'POST'):
+        print(request.data)
+        return "DATA Received"
+    else:
+        print("GET")
+        return "GET data"
+
+@app.route("/data", methods=['GET', 'POST'])
+def data_receive():
     if(request.method == 'POST'):
         store_signals(request.data)
         print(request.data)
@@ -28,29 +40,63 @@ def refresh_map():
     return app.send_static_file('svg_map.html')
 
 def store_signals(data):
-    f = open("wifi_signals.txt", "w")
+    global mac
+    global dB
+
     data_string = data.decode("utf-8")
+
     mac_idx  = 0
     dB_idx   = 0
-    mac_list = ["94:B4:0F:E6:35", "94:B4:0F:E6:45:C", "94:B4:0F:E6:44:E",
-     "E8:26:89:37:9B", "E8:26:89:37:DB", "94:B4:0F:E6:45:E", "F0:5C:19:6E:A0",
-     "94:B4:0F:E6:5F", "F0:5C:19:6E:74"]
     # Find mac and dB for each signal
     while(mac_idx >= 0 and dB_idx >= 0):
+
+        # find the mac address
         mac_idx = data_string.find('MAC:')
         mac_address = data_string[mac_idx+len("MAC:"):mac_idx+len("MAC:")+16]
         data_string = data_string[mac_idx+len("MAC:")+16:]
-        f.write(mac_address+" ")
+        
+        if(len(mac) == 10):
+            mac.popleft()
+        mac.append(mac_address)
+
+        # find the dB strength
         dB_idx = data_string.find('dB:')
         dB_string = data_string[dB_idx+len("dB:"):dB_idx+len("dB:")+3]
+
+        if(len(dB) == 10):
+            dB.popleft()
+
         if(dB_string == "100"):
-            f.write(dB_string + " ")
+            dB.append(dB_string)
         else:
-            f.write(data_string[dB_idx+len("dB:"):dB_idx+len("dB:")+2] + " ")
+            dB.append(data_string[dB_idx+len("dB:"):dB_idx+len("dB:")+2])
         data_string = data_string[dB_idx+len("dB:")+2:]
         mac_idx = data_string.find('MAC:')
-        f.write('\n')
-    f.close()
+    
+    f = open("wifi_signals.txt", "w")
+    for i in range(len(mac)):
+        f.write(mac[i]+' '+dB[i]+'\n')
+    f.close() 
+    
+    # data_string = data.decode("utf-8")
+    # mac_idx  = 0
+    # dB_idx   = 0
+    # # Find mac and dB for each signal
+    # while(mac_idx >= 0 and dB_idx >= 0):
+    #     mac_idx = data_string.find('MAC:')
+    #     mac_address = data_string[mac_idx+len("MAC:"):mac_idx+len("MAC:")+16]
+    #     data_string = data_string[mac_idx+len("MAC:")+16:]
+    #     f.write(mac_address+" ")
+    #     dB_idx = data_string.find('dB:')
+    #     dB_string = data_string[dB_idx+len("dB:"):dB_idx+len("dB:")+3]
+    #     if(dB_string == "100"):
+    #         f.write(dB_string + " ")
+    #     else:
+    #         f.write(data_string[dB_idx+len("dB:"):dB_idx+len("dB:")+2] + " ")
+    #     data_string = data_string[dB_idx+len("dB:")+2:]
+    #     mac_idx = data_string.find('MAC:')
+    #     f.write('\n')
+    # f.close()
 
 if __name__ == '__main__':
     open('wifi_signals.txt', 'w').close()
